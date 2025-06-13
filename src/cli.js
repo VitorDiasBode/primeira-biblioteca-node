25bd72a418e7c5b1746ffaeffcf2e2c9131aee1a
Vitor <vitordiasalves@gmail.com>
2025-06-13 15:44:44 -0300

    feat: Salva arquivo txt com o resultado da contagem de palavras

--- src/cli.js
+++ src/cli.js
@@ -4,12 +4,25 @@ import {contaPalavras} from './index.js';
 
 const caminhoDoArquivo = process.argv;
 const link = caminhoDoArquivo[2];
+const endereco = caminhoDoArquivo[3];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
     try{
-        if (erro) throw(erro);
-        console.log(contaPalavras(texto));
+        if (erro) throw erro;
+        const resultado = contaPalavras(texto);
+        criaESalvaPalavra(resultado, endereco);
     }catch(erro){
         trataErros(erro)
     }
 })
+
+async function criaESalvaPalavra(listaPalavras, endereco){
+    const arquivoNovo = `${endereco}/resultado.txt`;
+    const textoPalavras = JSON.stringify(listaPalavras);
+    try{
+        await fs.promises.writeFile(arquivoNovo, textoPalavras);
+        console.log('Arquivo criado');
+    }catch (erro){
+        throw erro
+    }
+}
